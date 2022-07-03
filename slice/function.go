4e1f1011263279d337e8b0562910b964982b208a
ngicks <yknt.bsl@gmail.com>
2022-07-03 16:12:36 +0000

    refactor(slice): match interface and naming convention to Rust

--- slice/function.go
+++ slice/function.go
@@ -1,47 +1,43 @@
 package slice
 
+// This file sticks its naming convention to it of Rust:
+//   - https://doc.rust-lang.org/std/iter/trait.Iterator.html
+//   - https://doc.rust-lang.org/std/vec/struct.Vec.html
+
 func Has[T comparable](sl []T, target T) bool {
-	return FindIndex(sl, target) >= 0
+	return Position(sl, func(v T) bool { return v == target }) >= 0
 }
-func Find[T comparable](sl []T, target T) (v T, found bool) {
-	if sl == nil || len(sl) == 0 {
+func Find[T any](sl []T, predicate func(v T) bool) (v T, found bool) {
+	p := Position(sl, predicate)
+	if p < 0 {
 		return
 	}
-	for _, v := range sl {
-		if target == v {
-			return v, true
-		}
-	}
-	return
+	return sl[p], true
 }
-func FindLast[T comparable](sl []T, target T) (v T, found bool) {
-	if sl == nil || len(sl) == 0 {
+func FindLast[T any](sl []T, predicate func(v T) bool) (v T, found bool) {
+	p := PositionLast(sl, predicate)
+	if p < 0 {
 		return
 	}
-	for i := len(sl) - 1; i >= 0; i-- {
-		if target == sl[i] {
-			return sl[i], true
-		}
-	}
-	return
+	return sl[p], true
 }
-func FindIndex[T comparable](sl []T, target T) int {
-	if sl == nil || len(sl) == 0 {
+func Position[T any](sl []T, predicate func(v T) bool) int {
+	if sl == nil || len(sl) == 0 || predicate == nil {
 		return -1
 	}
 	for idx, v := range sl {
-		if target == v {
+		if predicate(v) {
 			return idx
 		}
 	}
 	return -1
 }
-func FindIndexLast[T comparable](sl []T, target T) int {
-	if sl == nil || len(sl) == 0 {
+func PositionLast[T any](sl []T, predicate func(v T) bool) int {
+	if sl == nil || len(sl) == 0 || predicate == nil {
 		return -1
 	}
 	for i := len(sl) - 1; i >= 0; i-- {
-		if target == sl[i] {
+		if predicate(sl[i]) {
 			return i
 		}
 	}
--- slice/function_test.go
+++ slice/function_test.go
@@ -15,10 +15,71 @@ func TestFunc(t *testing.T) {
 	if slice.Has(sl, 10) {
 		t.Fatalf("must be false")
 	}
-	if slice.FindIndex(sl, 1) != 0 {
-		t.Fatalf("must be 0, but %d", slice.FindIndex(sl, 1))
+	if slice.Has(nil, 10) {
+		t.Fatalf("returned value must be false if input slice is nil")
 	}
-	if slice.FindIndexLast(sl, 1) != 4 {
-		t.Fatalf("must be 4, but %d", slice.FindIndexLast(sl, 1))
+
+	if idx, ok := slice.Find(sl, func(v int) bool { return v > 2 }); !ok || idx != 3 {
+		t.Fatalf("index must be %d, but %d", 3, idx)
+	}
+	if idx, ok := slice.Find(sl, func(v int) bool { return v == 6 }); ok {
+		t.Fatalf("returned ok must be false if no value matches to given predicate, but found %d", idx)
+	}
+	if _, ok := slice.Find(nil, func(v int) bool { return v == 1 }); ok {
+		t.Fatalf("returned ok must be false if any of input is nil")
+	}
+	if _, ok := slice.Find(sl, nil); ok {
+		t.Fatalf("returned ok must be false if any of input is nil")
+	}
+	if _, ok := slice.Find[int](nil, nil); ok {
+		t.Fatalf("returned ok must be false if any of input is nil")
+	}
+
+	if idx, ok := slice.FindLast(sl, func(v int) bool { return v > 2 }); !ok || idx != 5 {
+		t.Fatalf("index must be %d, but %d", 5, idx)
+	}
+	if idx, ok := slice.FindLast(sl, func(v int) bool { return v == 6 }); ok {
+		t.Fatalf("returned ok must be false if no value matches to given predicate, but found %d", idx)
+	}
+	if _, ok := slice.FindLast(nil, func(v int) bool { return v == 1 }); ok {
+		t.Fatalf("returned ok must be false if any of input is nil")
+	}
+	if _, ok := slice.FindLast(sl, nil); ok {
+		t.Fatalf("returned ok must be false if any of input is nil")
+	}
+	if _, ok := slice.FindLast[int](nil, nil); ok {
+		t.Fatalf("returned ok must be false if any of input is nil")
+	}
+
+	if p := slice.Position(sl, func(v int) bool { return v == 1 }); p != 0 {
+		t.Fatalf("must be 0, but %d", p)
+	}
+	if p := slice.Position(sl, func(v int) bool { return v == 6 }); p != -1 {
+		t.Fatalf("must be -1, but %d", p)
+	}
+	if p := slice.Position(nil, func(v int) bool { return v == 1 }); p != -1 {
+		t.Fatalf("must be -1, but %d", p)
+	}
+	if p := slice.Position(sl, nil); p != -1 {
+		t.Fatalf("must be -1, but %d", p)
+	}
+	if p := slice.Position[int](nil, nil); p != -1 {
+		t.Fatalf("must be -1, but %d", p)
+	}
+
+	if p := slice.PositionLast(sl, func(v int) bool { return v == 1 }); p != 4 {
+		t.Fatalf("must be 4, but %d", p)
+	}
+	if p := slice.PositionLast(sl, func(v int) bool { return v == 6 }); p != -1 {
+		t.Fatalf("must be -1, but %d", p)
+	}
+	if p := slice.PositionLast(nil, func(v int) bool { return v == 1 }); p != -1 {
+		t.Fatalf("must be -1, but %d", p)
+	}
+	if p := slice.PositionLast(sl, nil); p != -1 {
+		t.Fatalf("must be -1, but %d", p)
+	}
+	if p := slice.PositionLast[int](nil, nil); p != -1 {
+		t.Fatalf("must be -1, but %d", p)
 	}
 }
