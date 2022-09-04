89596f0ea3fad167fae2eb44ea856b41d401824d
ngicks <yknt.bsl@gmail.com>
2022-09-04 18:53:36 +0000

    feat: addfilterable heap
    and also moved test.

---
+++ filterable_heap.go
@@ -0,0 +1,79 @@
+package typeparamcommon
+
+type Lessable[T any] interface {
+	Inner() T
+	// LessThan determines self is less than input Lessable[T].
+	LessThan(Lessable[T]) bool
+}
+
+type FilterableHeap[U any, T Lessable[U]] struct {
+	*HeapWrapper[T]
+	internal *SliceInterface[T]
+}
+
+func genericLess[U any, T Lessable[U]](i T, j T) bool {
+	return i.LessThan(j)
+}
+
+func NewFilterableHeap[U any, T Lessable[U]]() *FilterableHeap[U, T] {
+	heapInternal, interfaceInternal := MakeHeap(genericLess[U, T])
+	return &FilterableHeap[U, T]{
+		HeapWrapper: heapInternal,
+		internal:    interfaceInternal,
+	}
+}
+
+// Peek returns most prioritized value in heap without removing it.
+// If this heap contains 0 element, returned p is zero value for type T.
+//
+// The complexity is O(1).
+func (h *FilterableHeap[U, T]) Peek() (p T) {
+	if len(h.internal.Inner) == 0 {
+		return
+	}
+	return h.internal.Inner[0]
+}
+
+func (h *FilterableHeap[U, T]) Len() int {
+	return h.internal.Len()
+}
+
+// Filter calls filterFuncs one by one, in given order, with following heap.Init().
+// Each filterFunc receives innerSlice so that it can be mutated in that func.
+//
+// Filter calls heap.Init() at the end of the method.
+// So the complexity is at least O(n) where n is h.Len().
+func (h *FilterableHeap[U, T]) Filter(filterFuncs ...func(innerSlice *[]T)) {
+	for _, v := range filterFuncs {
+		if v != nil {
+			v(&h.internal.Inner)
+		}
+	}
+
+	h.Init()
+}
+
+func BuildExcludeFilter[T any](start, end int, filterPredicate func(T) bool) func(sl *[]T) {
+	return func(sl *[]T) {
+		if start < 0 {
+			start = 0
+		} else if start >= len(*sl) {
+			return
+		}
+		if end > len(*sl) {
+			end = len(*sl)
+		}
+
+		if start > end {
+			return
+		}
+
+		for i := start; i < end; i++ {
+			if filterPredicate((*sl)[i]) {
+				*sl = append((*sl)[:i], (*sl)[i+1:]...)
+				end--
+				i--
+			}
+		}
+	}
+}
---
+++ filterable_heap_test.go
@@ -0,0 +1,101 @@
+package typeparamcommon_test
+
+import (
+	"testing"
+
+	typeparamcommon "github.com/ngicks/type-param-common"
+)
+
+type Int int
+
+func (i Int) Inner() Int {
+	return i
+}
+func (i Int) LessThan(j typeparamcommon.Lessable[Int]) bool {
+	return i < j.Inner()
+}
+
+func TestHeapWithAdditionalProps(t *testing.T) {
+	t.Run("Exclude", func(t *testing.T) {
+		h := typeparamcommon.NewFilterableHeap[Int, typeparamcommon.Lessable[Int]]()
+
+		h.Push(Int(7))
+		h.Push(Int(4))
+		h.Push(Int(1))
+		h.Push(Int(6))
+		h.Push(Int(5))
+		h.Push(Int(3))
+		h.Push(Int(2))
+
+		execlude := typeparamcommon.BuildExcludeFilter(
+			-1,
+			100,
+			func(ent typeparamcommon.Lessable[Int]) bool { return ent.Inner()%2 == 0 },
+		)
+
+		lenBefore := h.Len()
+		h.Filter(execlude)
+		removed := lenBefore - h.Len()
+
+		if removed != 3 {
+			t.Fatalf("removed len must be %d, but is %d", 3, removed)
+		}
+
+		for i := 1; i <= 7; i = i + 2 {
+			popped := h.Pop()
+			if int(popped.Inner()) != i {
+				t.Errorf("pop = %v expected %v", popped, i)
+			}
+		}
+
+		if h.Len() != 0 {
+			t.Errorf("expect empty but size = %v", h.Len())
+		}
+
+		h.Push(Int(7))
+		h.Push(Int(4))
+		h.Push(Int(1))
+		h.Push(Int(6))
+		h.Push(Int(5))
+		h.Push(Int(3))
+		h.Push(Int(2))
+
+		execlude = typeparamcommon.BuildExcludeFilter(
+			0,
+			3,
+			func(ent typeparamcommon.Lessable[Int]) bool { return ent.Inner()%2 == 0 },
+		)
+
+		lenBefore = h.Len()
+		h.Filter(execlude)
+		removed = lenBefore - h.Len()
+
+		if removed != 1 {
+			t.Fatalf("removed len must be %d, but is %d", 3, removed)
+		}
+
+		for h.Len() != 0 {
+			h.Pop()
+		}
+
+		h.Push(Int(7))
+		h.Push(Int(4))
+		h.Push(Int(1))
+		h.Push(Int(6))
+		h.Push(Int(5))
+		h.Push(Int(3))
+		h.Push(Int(2))
+
+		execlude = typeparamcommon.BuildExcludeFilter(
+			3,
+			6,
+			func(ent typeparamcommon.Lessable[Int]) bool { return ent.Inner()%2 == 0 },
+		)
+		lenBefore = h.Len()
+		h.Filter(execlude)
+		removed = lenBefore - h.Len()
+		if removed != 2 {
+			t.Fatalf("removed len must be %d, but is %d", 3, removed)
+		}
+	})
+}
