f13e45008fc6b459af3449f44b1a7532f47d6952
ngicks <yknt.bsl@gmail.com>
2022-12-12 11:18:39 +0000

    feat: add Assert

--- util/function.go
+++ util/function.go
@@ -1,3 +1,4 @@
+// util implements trivial util functions.
 package util
 
 // Must unwraps error probability from return value of functions
@@ -11,7 +12,7 @@ func Must[T any](val T, err error) T {
 	return val
 }
 
-// Must3 is same as Must but take 3 args.
+// Must3 is same as Must but takes 3 args.
 //
 // It panics if err is non nil.
 func Must3[T any, U any](val1 T, val2 U, err error) (T, U) {
@@ -26,4 +27,23 @@ func Must3[T any, U any](val1 T, val2 U, err error) (T, U) {
 // It is useful when setting built-in type T (e.g. string, int) to struct fields of *T.
 func Escape[T any](v T) *T {
 	return &v
+}
+
+// Assert does type assertion on v.
+// v is asserted against T and *T.
+// Assert returns true ok when v is either of T or *T.
+// If v is nil *T, then asserted is zero value of T.
+func Assert[T any](v any) (asserted T, ok bool) {
+	asserted, ok = v.(T)
+	if !ok {
+		p, ok := v.(*T)
+		if !ok {
+			return asserted, false
+		}
+		if p != nil {
+			asserted = *p
+		}
+	}
+
+	return asserted, true
 }
